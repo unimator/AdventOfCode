8aed001777297cfeb797ec11ab3532c286b8fe26
Marcin Górka <marcin.gorka@simployer.com>
2022-12-06 12:12:34 +0100

    day5 solution improvement

--- day5/solution.py
+++ day5/solution.py
@@ -1,41 +1,49 @@
-state_a = []
-state_b = []
+import re
 
-#input file has been parsed and split in two by hand
-
-with open('input_initial', 'r') as input_file:
+stacks_a = [[] for _ in range(9)]
+stacks_b = []
+N = 4
+GROUPS_NUM = 9
+with open('input', 'r') as input_file:
     lines = [line for line in input_file]
-    for line in lines:
-        stack_a = []
-        stack_b = []
-        for c in line.strip():
-            stack_a.append(c)
-            stack_b.append(c)
-        state_a.append(stack_a)
-        state_b.append(stack_b)
-
-with open('input_steps', 'r') as input_file:
-    lines = [line for line in input_file]
-    for line in lines:
-        [steps_count, take_from, put_on] = [int(x) for x in line.split(' ')]
+    line_it = iter(lines)
+    while not line.startswith(' 1'):
+        line = next(line_it)
+        line_splitted = [line[i:i+N] for i in range(0, len(line), N)]
+        for val in range(GROUPS_NUM):
+            c = line_splitted[val][1]
+            if ord(c) >= ord('A') and ord(c) <= ord('Z'):
+                stacks_a[val].append(c)
+    for stack in stacks_a:
+        stack.reverse()
+    stacks_b = [stack[:] for stack in stacks_a]
+    next(line_it)
+    pattern = "move (\d+) from (\d+) to (\d+)"
+    while True:
+        line = next(line_it, None)
+        if line == None:
+            break
+        res = re.match(pattern, line)
+        [steps_count, take_from, put_on] = [int(res.group(1)), int(res.group(2)), int(res.group(3))]
         for step in range(steps_count):
-            source = state_a[take_from - 1]
-            target = state_a[put_on - 1]
+            source = stacks_a[take_from - 1]
+            target = stacks_a[put_on - 1]
             current = source.pop()
             target.append(current)
         
-        source = state_b[take_from - 1]
-        target = state_b[put_on - 1]
+        source = stacks_b[take_from - 1]
+        target = stacks_b[put_on - 1]
         [leave, take] = [source[:-steps_count], source[-steps_count:]]
-        state_b[take_from - 1] = leave
-        state_b[put_on - 1] = target + take
+        stacks_b[take_from - 1] = leave
+        stacks_b[put_on - 1] = target + take
+
 
 task_a_result = ''
 task_b_result = ''
-for stack_a in state_a:
+for stack_a in stacks_a:
     task_a_result += stack_a[-1]
 
-for stack_b in state_b:
+for stack_b in stacks_b:
     task_b_result += stack_b[-1]
 
 # Task A
