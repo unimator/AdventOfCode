12e5fe0f3ce3a4e12c121d369e44461dcbb83e31
Marcin Górka <unimator@gazeta.pl>
2022-12-21 18:52:06 +0100

    day21 - part2 solution

--- 2022/day21/solution.py
+++ 2022/day21/solution.py
@@ -6,7 +6,7 @@ task_b_monkeys_unknown = {}
 
 def isnumber(str):
     try:
-        int(str)
+        float(str)
     except ValueError:
         return False
     else:
@@ -14,23 +14,23 @@ def isnumber(str):
 
 root_monkey = None
 
-with open('input', 'r') as input_file:
+with open('2022/day21/input', 'r') as input_file:
     for line in input_file:
         [monkey, operation] = line.split(':')
         operation = operation.strip()
         if isnumber(operation):
-            task_a_monkeys_known[monkey] = int(operation)
-            task_b_monkeys_known[monkey] = int(operation)
+            task_a_monkeys_known[monkey] = float(operation)
+            task_b_monkeys_known[monkey] = float(operation)
         else:
             [left, op, right] = operation.split(' ')
             task_a_monkeys_unknown[monkey] = (left, op, right)
             task_b_monkeys_unknown[monkey] = (left, op, right)
 
-task_b_monkeys_known['humn'] = 'X'
-(root_l, root_op, root_r) = task_b_monkeys_unknown['root']
-task_b_monkeys_unknown['root'] = (root_l, '=', root_r)
+task_b_monkeys_known['humn'] = 'X' # 3678125418015
+(root_l, root_op, root_r) = task_b_monkeys_unknown['root'] # for my nput root_l is an expression and root_r is some calculated value
+del task_b_monkeys_unknown['root']
 
-def evaluate(left, right, op):
+def evaluate_a(left, right, op):
     if op == '+':
         return left + right
     if op == '-':
@@ -47,13 +47,13 @@ while len(task_a_monkeys_unknown) > 0:
         if left not in task_a_monkeys_known or right not in task_a_monkeys_known:
             continue
         else:
-            task_a_monkeys_known[monkey_unknown] = evaluate(task_a_monkeys_known[left], task_a_monkeys_known[right], op)
+            task_a_monkeys_known[monkey_unknown] = evaluate_a(task_a_monkeys_known[left], task_a_monkeys_known[right], op)
     for monkey_known in task_a_monkeys_known:
         if monkey_known in task_a_monkeys_unknown:
             del task_a_monkeys_unknown[monkey_known]
 
 # Task A
-print(task_a_monkeys_known['root'])
+print(int(task_a_monkeys_known['root']))
 
 def evaluate_b(left, right, op):
     if not isnumber(left) or not isnumber(right):
@@ -76,28 +76,47 @@ while len(task_b_monkeys_unknown) > 0:
         else:
             left = task_b_monkeys_known[left] if left in task_b_monkeys_known else left
             right = task_b_monkeys_known[right] if right in task_b_monkeys_known else right
-            if not isnumber(right):
-                tmp = left
-                left = right
-                right = tmp
             task_b_monkeys_known[monkey_unknown] = evaluate_b(left, right, op)
     for monkey_known in task_b_monkeys_known:
         if monkey_known in task_b_monkeys_unknown:
             del task_b_monkeys_unknown[monkey_known]
 
-print(task_b_monkeys_known['root'])
+expr = task_b_monkeys_known[root_l] + '-' + str(task_b_monkeys_known[root_r])
 
-# (Y - 116154256834924) * 2 = 118841721834931
 
-lX = -1000000000000000
-rX = 1000000000000000
+# Rough approximation
+task_b_result = None
+best_expr_val = None
+min_X = -1000000000000000
+max_X = 1000000000000000
 while True:
-    expr = '(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((X + 816) + 497) / 3) - 395) * 45) + 560) * 2) - 671) + 794) + 697) / 5) + 558) * 4) - 284) / 10) - 428) / 3) + 438) * 32) + 984) / 8) - 802) * 10) + 694) / 2) - 486) * 2) + 97) * 2) - 161) + 958) / 3) - 384) * 2) + 928) / 2) + 633) / 2) - 773) * 2) - 19) * 2) + 904) / 10) - 59) * 8) + 337) + 389) / 6) - 146) * 3) + 446) + 918) / 5) - 606) * 11) + 228) + 746) + 477) / 5) - 638) / 5) + 628) * 2) - 792) / 2) + 298) * 22) + 146) / 2) - 118841721834931) * 2))'
-    # expr_lX = expr.replace('X', lX)
-    # expr_rX = expr.replace('X', rX)
-    # expr_l = eval(expr_lX)
-    # expr_r = eval(expr_rX)
-    # l_dist = abs(expr_l)
-    # r_dist = abs(expr_r)
-    # if r_dist > l_dist:
-    #     rX = 
+    expr_min_X = expr.replace('X', str(min_X))
+    expr_max_X = expr.replace('X', str(max_X))
+    min_X_value = eval(expr_min_X)
+    max_X_value = eval(expr_max_X)
+    min_dist = abs(min_X_value)
+    max_dist = abs(max_X_value)
+    if abs(min_X_value) < 1:
+        task_b_result = int(min_X)
+        best_expr_val = min_X_value
+        break
+    elif abs(max_X_value) < 1:
+        task_b_result = int(max_X)
+        best_expr_val = max_X_value
+        break
+    if min_dist < max_dist:
+        max_X = (min_X + max_X) / 2
+    else:
+        min_X = (min_X + max_X) / 2
+
+# Tunning
+
+best_i = 0
+for i in range(-10000, 10000):
+    expr_val = eval(expr.replace('X', str(task_b_result + i)))
+    if abs(expr_val) < abs(best_expr_val):
+        best_expr_val = expr_val
+        best_i = i
+
+# Task B
+print(task_b_result + best_i)
